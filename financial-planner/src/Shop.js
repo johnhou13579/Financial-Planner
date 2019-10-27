135995e06b3c3b9e20d9beb9b23c9437f83ec6c5
unknown <houj@oxy.edu>
2019-10-27 00:29:10 -0700

    Used test get api to map items onto shop page

--- financial-planner/src/Shop.js
+++ financial-planner/src/Shop.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function Shop() {
+    
+    useEffect(()=>{
+        fetchItems();
+    },[]);
+
+    const [items, setItems] = useState([]);
+
+    const fetchItems= async () => {
+        const data = await fetch('https://fortnite-api.theapinetwork.com/upcoming/get');
+        
+        const items = await data.json();
+        console.log(items.items);
+        setItems(items.items);
+    }
+
   return (
     <div>
-      <h1>App</h1>
+      {items.map(item=>(
+          <h1 key={item.itemid}>{item.name}</h1>
+      ))}
     </div>
   );
 }
