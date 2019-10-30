aff3b67482c8d85b7b2419afba0dfb0f17d202a0
unknown <houj@oxy.edu>
2019-10-30 10:59:07 -0700

    Added api for jobs

--- financial-planner/src/components/Shop.js
+++ financial-planner/src/components/Shop.js
@@ -15,7 +15,7 @@ function Shop() {
     const [items, setItems] = useState([]);
 
     const fetchItems = async () => {
-        const data = await fetch('https://fortnite-api.theapinetwork.com/upcoming/get');
+        const data = await fetch('https://jobs.github.com/positions.json?search=node');
 
         const items = await data.json();
         console.log(items.items);
